694cd8b28dfc42d1be4531a86a1fca2ced8d6311
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-10-07 09:02:45 +0530

    inital commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose')
+
+
+
+const clusterMongoDb = "mongodb+srv://testuser:testShinde@cluster0.shr0h.mongodb.net/bookmark-api?retryWrites=true&w=majority"
+
+mongoose.connect(clusterMongoDb,
+    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
+    (err) => {
+        console.log("connected", err ? err : true)
+    }
+)
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
