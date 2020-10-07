ef39d5c48e2c185ee85d6a8601545700a1ceda6f
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-10-07 17:58:35 +0530

    create tag

--- routes/tags.js
+++ routes/tags.js
@@ -1,7 +1,21 @@
 var express = require('express')
 var router = express.Router()
+var Tags = require('../models/tags')
+var Bookmark = require('../models/bookmark')
 
-
+// create tag
+router.post('/create', async (req, res, next) => {
+    try {
+        var tag = await Tags.create(req.body.tag)
+        res.status(201).json({
+            tag: {
+                title: tag.title
+            }
+        })
+    } catch (error) {
+        next(error)
+    }
+})
 
 
 module.exports = router
